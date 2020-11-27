3c8b188283035938f16e08b2405db536e7ffdbbd
berryboylb <phemmynesce4life@gmail.com>
2020-11-27 13:44:28 +0000

    ...

--- scripts/animate.js
+++ scripts/animate.js
@@ -11,69 +11,63 @@ tl.from('nav', {y:-100, opacity: 0, ease: 'bounce',})
     .from('.second-sec-in button', {x: -200, opacity: 0, ease: 'back', }, "-=1.5")
     
     
-    gsap.from(".People-image", {
-        scrollTrigger: {trigger:".second-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+    gsap.from(".second-sec .second-sec-div1 .People-image", {
+        scrollTrigger: {trigger:".second-sec-div1", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
         x: 300, ease: 'back', duration: 3, opacity: 0
       });
 
-      gsap.from(".second-sec-div2 div", {
-        scrollTrigger: {trigger:".second-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
-        x: -300, ease: 'back', duration: 3, opacity: 0
-      });
-
-      gsap.from(".second-sec .flower-img", {
-        scrollTrigger: {trigger:".second-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+      gsap.from(".second-sec-div2 .about", {
+        scrollTrigger: {trigger:".about", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
         x: -300, ease: 'back', duration: 3, opacity: 0
       });
 
       gsap.from(".third-sec h2", {
-        scrollTrigger: {trigger:".third-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".third-sec", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
         x: 300, y: 200, ease: 'Rough', duration: 1, opacity: 0, scale : 2
       });
 
       gsap.from(".third-sec .third-sec-in div", {
-        scrollTrigger: {trigger:".third-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".third-sec .third-sec-in div", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'elastic', duration: 2, opacity: 0, scale: 2, stagger: 1
       });
 
       gsap.from(".fourth-sec .fourth-sec-1", {
-        scrollTrigger: {trigger:".fourth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".fourth-sec-1", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'back', duration: 2, opacity: 0, x: -200, 
       });
 
       gsap.from(".fourth-sec .fourth-sec-2", {
-        scrollTrigger: {trigger:".fourth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".fourth-sec-2", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'back', duration: 2, opacity: 0, x: 200, 
       });
 
       gsap.from(".fourth-sec .fourth-sec-3", {
-        scrollTrigger: {trigger:".fourth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".fourth-sec-3", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'back', duration: 2, opacity: 0, x: -200, 
       });
 
       gsap.from(".fifth-sec h1", {
-        scrollTrigger: {trigger:".fifth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".fifth-sec h1", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'elastic', duration: 2, opacity: 0, x: -200, 
       });
 
       gsap.from(".fifth-sec button", {
-        scrollTrigger: {trigger:".fifth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".fifth-sec button", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'elastic', duration: 2, opacity: 0, x: 200, 
       });
 
       gsap.from(".fifth-sec .fifth-sec-in p", {
-        scrollTrigger: {trigger:".fifth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".fifth-sec-in p", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'elastic', duration: 2, opacity: 0, x: 200, stagger: 0.5
       });
 
-
       gsap.from(".sixth-sec .sixth-sec-1 img", {
-        scrollTrigger: {trigger:".sixth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".sixth-sec-1 img", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'elastic', duration: 2, opacity: 0, x: -200, 
       });
 
       gsap.from(".sixth-sec .sixth-sec-2", {
-        scrollTrigger: {trigger:".sixth-sec", toggleActions: "restart pause none none"}, // start the animation when ".box" enters the viewport (once)
+        scrollTrigger: {trigger:".sixth-sec-2", toggleActions: "restart none none none"}, // start the animation when ".box" enters the viewport (once)
          ease: 'elastic', duration: 2, opacity: 0, x: 200, 
       });
 
